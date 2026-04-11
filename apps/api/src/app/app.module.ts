523e2f1733ab241ae9a26897229aa8c161309548
dmytro-chushko <dmytro.chushko@gmail.com>
2026-04-11 23:54:36 +0300

    Move general module to the app directory

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 
+import { ChatModule } from '../chat/chat.module';
+import { CommonModule } from '../common/common.module';
+import { AllExceptionsFilter } from '../filters/all-exceptions';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatModule } from './chat/chat.module';
-import { CommonModule } from './common/common.module';
-import { AllExceptionsFilter } from './filters/all-exceptions';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule, ChatModule],
